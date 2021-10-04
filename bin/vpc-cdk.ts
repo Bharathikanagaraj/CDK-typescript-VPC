5ba9f1e42d99fe0f2dbe6b475d91b50409fc4449
Bharathikanagaraj <91605072+Bharathikanagaraj@users.noreply.github.com>
2021-10-04 18:24:52 +0530

    Lamnda

--- bin/vpc-cdk.ts
+++ bin/vpc-cdk.ts
@@ -4,8 +4,11 @@ import * as cdk from '@aws-cdk/core';
 import { VpcCdkStack } from '../lib/vpc-cdk-stack';
 import { IAMCdkStack } from '../lib/IAM-cdk-stack';
 import { EC2RDSStack } from '../lib/EC2 RDS- stack';
+import { LambdaStack } from '../lib/Lambda-stack';
 
 const app = new cdk.App();
 new VpcCdkStack(app, 'VpcCdkStack', {});
 new IAMCdkStack(app, 'IamCdkStack', {});
 new EC2RDSStack(app, 'EC2RDSStack', {});
+new LambdaStack(app, 'LambdaStack', {});
+
